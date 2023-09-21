7821400cd0612157f0eda4b5e6e34bb63f404e22
Toydrum <61118847+Toydrum@users.noreply.github.com>
2023-09-21 16:33:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.get('/api/products', async(req,res)=>{
     try {
-        const products = await productsManager.getProducts()
+        const products = await productsManager.getProducts(req.query)
         if(!products.length){
             return res.status(200).json({message: "no products"})
         }
