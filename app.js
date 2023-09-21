8613a7d7c5ea9c2c044e8b5ef26bd646245c0d52
Toydrum <61118847+Toydrum@users.noreply.github.com>
2023-09-21 16:32:23 -0500

    completo

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { productsManager } from './ProductsManager.js';
+
+const app = express()
+
+app.get('/api/products', async(req,res)=>{
+    try {
+        const products = await productsManager.getProducts()
+        if(!products.length){
+            return res.status(200).json({message: "no products"})
+        }
+        res.status(200).json({message: "products found", products})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/products/:idProduct', async(req,res)=>{
+    const {idProduct} = req.params
+    try {
+        const product = await productsManager.getProductById(+idProduct)
+        if(!product){
+            return res.status(400).json({message: 'product not found with the id'})
+        }
+        res.status(200).json({message: "product found", product})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
+
+
+
+
+
+
+app.listen(8080,()=>{
+    console.log('Escuchando al puerto 8080')
+})
