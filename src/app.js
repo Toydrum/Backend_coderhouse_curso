d5f52934882f7b1cad8d6bbd3b31beea34c2a42c
Toydrum <61118847+Toydrum@users.noreply.github.com>
2023-09-24 21:00:45 -0500

    Entrega-

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productsRouter from './Router/Products.router.js'; 
+import cartRouter from './Router/Cart.router.js';
 import { __dirname } from './utils.js';
 
 
@@ -11,6 +12,7 @@ app.use(express.static(__dirname+'/public'))
 
 //routes
 app.use('/api/products', productsRouter);
+app.use('/api/cart', cartRouter)
 
 
 app.listen(8080,()=>{
