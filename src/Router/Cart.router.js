f803c3c35449fa397a14c54ea96e7a05fd458a98
Toydrum <61118847+Toydrum@users.noreply.github.com>
2023-09-27 15:15:10 -0500

    entrega

--- src/Router/Cart.router.js
+++ src/Router/Cart.router.js
@@ -1,12 +1,37 @@
 import { Router } from "express";
-
+import cartManager from "../CartManager.js";
+import express from "express";
 const router = Router();
-
-
-
-
-
-
-
-
+/* Create */
+router.post("/", async (req, res) => {
+    res.json(await cartManager.addCart(req.body));
+});
+router.post("/:cid/product/:pid", async (req, res) => {
+  res.json(
+    await cartManager.addProductToCart(req.params.cid, req.params.pid)
+  );
+});
+/* Read */
+router.get("/:cid", async (req, res) => {
+  res.json(await cartManager.getCartById(req.params.cid));
+});
+/* Update */
+router.put("/:cid", async (req, res) => {
+  res.json(await cartManager.updateCart(req.params.cid, req.body));
+});
+router.put("/:cid/product/:pid", async (req, res) => {
+  res.json(
+    await cartManager.updateProductFromCart(
+      req.params.cid,
+      req.params.pid,
+      req.body
+    )
+  );
+});
+/* Delete */
+router.delete("/:cid/product/:pid", async (req, res) => {
+  res.json(
+    await cartManager.deleteProductFromCart(req.params.cid, req.params.pid)
+  );
+});
 export default router;
